c9b869b85c949b3d6a87953b6cc256b79d6d975b
Henry001-prog <alissonhenrique001@hotmail.com.br>
2021-12-19 13:03:43 -0300

    unit tests with Jest

--- __tests__/integrations/api.test.js
+++ __tests__/integrations/api.test.js
@@ -1,3 +1,4 @@
+const { not } = require('expect');
 const Product = require('../../src/models/Product');
 
 describe("CRUD", () => {
@@ -6,93 +7,76 @@ describe("CRUD", () => {
     });
 
     it("Deverá criar um produto no banco de dados", async () => {
-        try {
-            const response = await Product.create({
-                title: 'Moto G6',
-                description: '64GB',
-                situationId: 3
-            });
         
-            //console.log(response);
-        
-            expect(response.title).toBe(response.title);
-        } catch (e) {
-            console.log('Ocorreu um erro ao criar o registro.');
-        }
+        const response = await Product.create({
+            title: 'Moto G4 Power',
+            description: '64GB',
+            situationId: 3
+        });
+
+        expect(response.title).not.toBeNull();
+        expect(response.title).toBeDefined();
     });
 
     it("Deverá trazer todos os produto no banco de dados", async () => {
-        try {
-            const response = await Product.findAll();
         
-            //console.log(response);
-        
-            expect(response.title).toBe(response.title);
-        } catch (e) {
-            console.log('Ocorreu um erro ao buscar todos os registros.');
-        }
+        const response = await Product.findAll();
+    
+        expect(response.title).not.toBeNull();
+        expect(response).toBeDefined();
     });
 
     it("Deverá trazer somente um produto do banco de dados", async () => {
-        try {
-            const response = await Product.findByPk(1);
         
-            //console.log(response);
-            if (response) {
-                expect(response.title).toBe(response.title);
-            } else {
-                console.log('Falhou!!!');
-            }
-            
-        } catch (e) {
-            console.log('Ocorreu um erro ao buscar o registro.');
+        const response = await Product.findByPk(1);
+    
+        //console.log(response);
+        if (response) {
+            expect(response.title).not.toBeNull();
+            expect(response.title).toBeDefined();
+        } else {
+            console.log('Falhou!!!');
         }
     });
 
     it("Deverá atualizar um produto no banco de dados", async () => {
-        try {
-            const result = await Product.findByPk(2);
-            if(result != null) {
-                const response = await result.update(
-                    { 
-                        title: 'Samsung Galaxy A70',
-                        description: '12GB',
-                        situationId: 1
-                    }, 
-                    {
-                    where: {
-                        id: 2
-                    }
-                });
-                expect(response.title).toBe(response.title);
-            } else {
-                console.log('Ocorreu um erro ao atualizar o registro.');
-            }
         
-            //console.log(response);
-        } catch (e) {
-            console.log('Ocorreu um erro ao atualizar o registro durante a execução da função.');
+        const result = await Product.findByPk(2);
+        if(result != null) {
+            const response = await result.update(
+                { 
+                    title: 'Samsung Galaxy A60',
+                    description: '12GB',
+                    situationId: 1
+                }, 
+                {
+                where: {
+                    id: 2
+                }
+            });
+            expect(response.title).not.toBeNull();
+            expect(response.title).toBeDefined();
+        } else {
+            console.log('Ocorreu um erro ao atualizar o registro.');
         }
     });
 
 
     it("Deverá deletar um produto no banco de dados", async () => {
-        try {
-            const response = await Product.destroy({
-                where: {
-                    id: 17
-                }
-            });
-            if (response !== 0) {
-                expect(response.title).toBe(response.title);
-            } else {
-                console.log('Não foi possível deletar esse registro solicitado!');
+        
+        const response = await Product.destroy({
+            where: {
+                id: 32
             }
-        } catch (e) {
-            console.log('Ocorreu um erro ao deletar o registro durante a execução da função.');
+        });
+        if (response !== 0) {
+            expect(response.title).not.toBeNull();
+        } else {
+            console.log('Não foi possível deletar esse registro solicitado!');
         }
     });
+
     afterAll(async () => {
         Product.sequelize.close();
-    })
+    });
 });
